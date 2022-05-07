2e4179a71c2d701e99abcfdf31c98c745336c9b4
Taha Enes Aslantürk <enes.aslanturk@ug.bilkent.edu.tr>
2022-05-07 15:50:56 +0300

    first upd

--- Whose Text/ViewController.swift
+++ Whose Text/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import CoreML
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var whoseTextField: UITextField!
+    @IBOutlet weak var whoseLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        whoseTextField.delegate = self
+        whoseTextField.returnKeyType = .done
     }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        whoseLabel.text = "???"
+        whoseLabel.textColor = .black
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        whoseTextField.resignFirstResponder()
+        makePrediciton()
+        return true
+    }
+    
+    
+    
 
+    @IBAction func findButtonPressed(_ sender: UIButton) {
+        makePrediciton()
+                
+    }
+    
+    func makePrediciton() {
+        if let predictText = whoseTextField.text {
+            do {
+                let textClassifier = try WhoseTextClassifier.init(configuration: MLModelConfiguration())
+                let prediction = try! textClassifier.prediction(text: predictText)
+                whoseLabel.text = prediction.label
+                whoseLabel.textColor = .systemGreen
+                whoseTextField.endEditing(true)
+            } catch {
+                print("Error when making prediciton \(error)")
+            }
+            
+        }
 
+        
+        
+    }
+    
 }
 
